8b7628765cb75269d199d6c942e7ad5b222cb885
JeanCarlos <jmatochepascual@gmail.com>
2024-05-03 21:27:52 -0500

    [FEATURE] mostrarNotificacion

--- js/app.js
+++ js/app.js
@@ -14,6 +14,36 @@ const citaObj = {
   sintomas: ''
 };
 
+//  Clases
+class Notificacion {
+  constructor({ mensaje, tipo = true }) {
+    this.mensaje = mensaje;
+    this.tipo = tipo;
+  }
+
+  mostrarNotificacion() {
+    const existeAlerta = document.querySelector('.alert');
+
+    if (!existeAlerta) {
+      const alerta = document.createElement('div');
+      alerta.textContent = this.mensaje;
+      alerta.classList.add('text-center', 'w-full', 'p-3', 'text-white', 'my-5', 'uppercase', 'font-bold', 'text-sm', 'alert');
+
+      this.tipo
+        ? alerta.classList.add('bg-green-500')
+        : alerta.classList.add('bg-red-500');
+
+      formulario.parentElement.insertBefore(alerta, formulario);
+
+      setTimeout(() => {
+        alerta.remove();
+      }, 3000);
+    }
+  }
+}
+
+let notificacion;
+
 
 // Llenar datos de la cita
 const llenarDatosCitaObj = (e) => {
@@ -26,8 +56,11 @@ const init = (event) => {
   event.preventDefault();
 
   if (Object.values(citaObj).includes('')) {
+    notificacion = new Notificacion({ mensaje: 'Todos los campos son obligatorios', tipo: false });
+    notificacion.mostrarNotificacion();
     return;
   };
+
 };
 
 // Cargar eventos
