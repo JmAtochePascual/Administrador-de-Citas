18de61c9d49854a0a7721fef513dab135e130ed3
JeanCarlos <jmatochepascual@gmail.com>
2024-05-05 08:43:41 -0500

    [FEATURE] agregar el modulo de selectores

---
+++ js/selectores.js
@@ -0,0 +1,8 @@
+export const paciente = document.querySelector('#paciente');
+export const propietario = document.querySelector('#propietario');
+export const email = document.querySelector('#email');
+export const fecha = document.querySelector('#fecha');
+export const sintomas = document.querySelector('#sintomas');
+export const formulario = document.querySelector('#formulario-cita');
+export const contenedorCitas = document.querySelector('#citas');
+
